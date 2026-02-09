761e1d34cfb0c1b74f35b85114b9600f0e843218
Sudarshan Bankar <sudarshanbankar9@gmail.com>
2026-02-10 02:45:43 +0530

    fix: add production origin to allowedOrigins for CORS

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 4000;
 
 
 // allow multiple origins
-const allowedOrigins=['http://localhost:5173']
+const allowedOrigins=['http://localhost:5173', 'https://suda-cart.vercel.app']
 
 app.post('/stripe', express.raw({type:'application/json'}), stripeWebHook)
 
